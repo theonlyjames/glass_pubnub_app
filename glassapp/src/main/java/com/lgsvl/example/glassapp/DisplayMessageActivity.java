a20883e1fdcd695357e9a4cc96972f4794415abd
james.khatiblou <james.khatiblou@lge.com>
2014-02-25 16:40:20 -0800

    added TuggableView subclass of CardScrollView

--- glassapp/src/main/java/com/lgsvl/example/glassapp/DisplayMessageActivity.java
+++ glassapp/src/main/java/com/lgsvl/example/glassapp/DisplayMessageActivity.java
@@ -6,20 +6,18 @@ import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.NavUtils;
 import android.view.MenuItem;
-import android.widget.ScrollView;
-
-import com.google.android.glass.app.Card;
-
-import java.util.List;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends Activity {
-
-    private List<Card> mCards;
-    private ScrollView mCardScrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        // was: setContentView(R.layout.main_activity);
+        setContentView(new TuggableView(this, R.layout.activity_display_message));
+
 
         // Make sure we're running on Honeycomb or higher to use ActionBar APIs
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
@@ -36,9 +34,9 @@ public class DisplayMessageActivity extends Activity {
 //        cardView.setText(message);
 //        cardView.setFootnote(message);
 //        cardView.toView();
-//
-//        TextView textDisplay = (TextView) findViewById(R.id.textDisplay);
-//        textDisplay.setText(message);
+
+        TextView textDisplay = (TextView) findViewById(R.id.textDisplay);
+        textDisplay.setText(message);
     }
 
     @Override
