7a2e8b30865eddcf878419dbd0a88d343006b8a6
james.khatiblou <james.khatiblou@lge.com>
2014-02-26 10:55:53 -0800

    Added dependencies to gradle file for connect sdk and imported most all of the code from ActivityMain from simple photo share

--- glassapp/src/main/java/com/lgsvl/example/glassapp/HomeActivity.java
+++ glassapp/src/main/java/com/lgsvl/example/glassapp/HomeActivity.java
@@ -1,22 +1,38 @@
 package com.lgsvl.example.glassapp;
 
 import android.app.Activity;
+import android.app.Dialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.glass.touchpad.Gesture;
 import com.google.android.glass.touchpad.GestureDetector;
 import com.pubnub.api.Pubnub;
 
+import com.connectsdk.core.LaunchSession;
+import com.connectsdk.device.ConnectableDevice;
+import com.connectsdk.device.ConnectableDeviceListener;
+import com.connectsdk.device.DevicePicker;
+import com.connectsdk.discovery.DiscoveryManager;
+import com.connectsdk.service.DeviceService.PairingType;
+import com.connectsdk.service.capability.listeners.LaunchListener;
+import com.connectsdk.service.command.ServiceCommandError;
+
 import org.json.JSONArray;
 import org.json.JSONObject;
 
 import java.util.Hashtable;
+import java.util.List;
 
 
 public class HomeActivity extends Activity {
@@ -27,36 +43,32 @@ public class HomeActivity extends Activity {
 
     private GestureDetector mGestureDetector;
 
+    DiscoveryManager _discoveryManager;
+    Dialog _pickerDialog;
+    ConnectableDevice _device;
+    TextView _statusTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
 
-//        Card card1;
-//        card1 = new Card(this);
-//        card1.setText("This card has a footer.");
-//        card1.setFootnote("I'm the footer!");
-//
-//        // Don't call this if you're using TimelineManager
-//        //View card1 View = card1.toView();
-//        card1.toView();
-
-        // new usage view
-        //u = new TextView(this);
-
-
-        //Intent intent = getIntent();
-        //String message = intent.getStringExtra(HomeActivity.EXTRA_MESSAGE);
-
-        // Create the text view
-        //TextView textView = new TextView(this);
-        //textView.setTextSize(40);
-        //textView.setText(message);
-
-        //setContentView(textView);
-
-
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        DiscoveryManager.getInstance(getApplicationContext()).start();
+
+        setupPicker();
+
+        _statusTextView = (TextView) this.findViewById(R.id.statusTextView);
+        Button shareImageButton = (Button) this.findViewById(R.id.shareImageButton);
+
+        shareImageButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                _pickerDialog.show();
+            }
+        });
 
         // new from PUBANO
         /*setContentView(R.layout.usage);
@@ -135,6 +147,95 @@ public class HomeActivity extends Activity {
           Log.d("PUBNUB",e.toString());
         }*/
 
+    }
+
+    private void setupPicker()
+    {
+        DiscoveryManager.getInstance(getApplicationContext()).registerDefaultDeviceTypes();
+
+        DevicePicker dp = new DevicePicker(this);
+        _pickerDialog = dp.getPickerDialog("Device List", new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+                                    long arg3) {
+                DiscoveryManager.getInstance(getApplicationContext()).stop();
+
+                _device = (ConnectableDevice)arg0.getItemAtPosition(arg2);
+                _device.addListener(deviceListener);
+                _device.connect();
+            }
+        });
+    }
+
+    private ConnectableDeviceListener deviceListener = new com.connectsdk.device.ConnectableDeviceListener() {
+
+        @Override
+        public void onPairingRequired(PairingType pairingType) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onDeviceReady() {
+            _device.getMediaPlayer().displayImage(
+                    "http://www.freesoftwaremagazine.com/files/nodes/3466/fig_sintel_style_study.jpg",
+                    "image/png",
+                    "Sintel",
+                    "Blender Open Movie Project",
+                    null,
+                    new LaunchListener() {
+
+                        @Override
+                        public void onLaunchSuccess(LaunchSession launchSession) {
+                            runOnUiThread(new Runnable() {
+
+                                @Override
+                                public void run() {
+                                    _statusTextView.setText("Successfully displayed image!");
+                                }
+                            });
+                        }
+
+                        @Override
+                        public void onLaunchFailed(ServiceCommandError error) {
+                            final String errorDesc = error.getDesc();
+
+                            runOnUiThread(new Runnable() {
+
+                                @Override
+                                public void run() {
+                                    _statusTextView.setText("An error occured while displaying image: " + errorDesc);
+                                }
+                            });
+                        }
+                    });
+        }
+
+        @Override
+        public void onDeviceDisconnected() {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onConnectionFailed(ServiceCommandError error) {
+            // TODO Auto-generated method stub
+
+        }
+
+        @Override
+        public void onCapabilityUpdated(List<String> added, List<String> removed) {
+            // TODO Auto-generated method stub
+
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.home, menu);
+        return true;
     }
 
     public void sendMessage(String messageNew) {
